a0272350db88462ad74e51a6d197aa4703900047
frederick tyteca <frederick@tyteca.net>
2021-09-08 18:40:21 +0200

    testDeal

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+const TOTAL_NUMBER_OF_CARDS = 16
+
 func TestGetRandomValue(t *testing.T) {
 	v := getRandomValue(10)
 	if !(0 <= v && v <= 10) {
@@ -14,7 +16,7 @@ func TestGetRandomValue(t *testing.T) {
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-	expectedNumberOfCards := 16
+	expectedNumberOfCards := TOTAL_NUMBER_OF_CARDS
 	expectedFirstCard := "As de Carreau"
 	expectedLastCard := "Trois de Trèfle"
 
@@ -28,5 +30,20 @@ func TestNewDeck(t *testing.T) {
 
 	if d[len(d)-1] != expectedLastCard {
 		t.Errorf("Expected last card was "+fmt.Sprint(expectedLastCard)+", obtained %v", d[len(d)-1])
+	}
+}
+
+func TestDeal(t *testing.T) {
+	d := newDeck()
+	expectedNumberOfCardsInDeck1 := 6
+	expectedNumberOfCardsInDeck2 := TOTAL_NUMBER_OF_CARDS - expectedNumberOfCardsInDeck1
+
+	deck1, deck2 := deal(d, expectedNumberOfCardsInDeck1)
+	if len(deck1) != expectedNumberOfCardsInDeck1 {
+		t.Errorf("Expected number of cards in deck 1 was "+fmt.Sprint(expectedNumberOfCardsInDeck1)+", obtained %v", len(deck1))
+	}
+
+	if len(deck2) != expectedNumberOfCardsInDeck2 {
+		t.Errorf("Expected number of cards in deck 2 was "+fmt.Sprint(expectedNumberOfCardsInDeck2)+", obtained %v", len(deck2))
 	}
 }
